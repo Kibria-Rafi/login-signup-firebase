0900e6562edea6a96ff32e536d81e2a4dd95115b
Kibria-Rafi <kibriarafi.bd.natore@gmail.com>
2023-10-02 16:20:21 +0600

    finally done login signup page by using firebase

--- src/Components/HeroRegeister/HeroRegister.jsx
+++ src/Components/HeroRegeister/HeroRegister.jsx
@@ -1,22 +1,32 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
 import { useState } from "react";
+import { FaEye,FaEyeSlash } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 const HeroRegister = () => {
     const [registerError,setRegisterError] =useState('')
     const [sucess,setsucess] =useState('')
+    const [showPassword,setShowPassword] = useState(false);
     const handleSubmit = e =>{
         e.preventDefault();
-
+        const name =e.target.name.value;
         const email =e.target.email.value;
         const password =e.target.password.value;
-        console.log(email,password);
+        const accepted = e.target.checkbox.checked;
+        console.log(email,password ,accepted,name);
         if(password.length <6){
             setRegisterError('The password must be 6 charector')
+            return;
         }
-        else if(!/A-Z/.test(password)){
+        else if(!/[A-Z]/.test(password)){
             setRegisterError("passwora must have uppercase")
         }
+        else if(!accepted){
+            setRegisterError('Please accept our terms and condition');
+            return;
+        }
+        //reset
         setRegisterError('');
         setsucess('')
         // create user
@@ -24,6 +34,19 @@ const HeroRegister = () => {
         .then(result =>{
             console.log(result.user);
             setsucess("user Create sucessfull")
+            // update profile
+            updateProfile(result.user,{
+              displayName:name,
+              
+            })
+            .then(()=> console.log('profile Updated'))
+            .catch(error=> console.error(error))
+            // sent verification
+            sendEmailVerification(result.user)
+            .then(()=>{
+                alert('plase check your email and verify your account')
+            })
+            
         })
         .catch(error =>{
             console.error(error);
@@ -36,11 +59,9 @@ const HeroRegister = () => {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Register Now</h1>
+            <h1 className="text-5xl font-bold">SignUp Now</h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              add your Email and set your secreat password
             </p>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -50,6 +71,14 @@ const HeroRegister = () => {
                   <label className="label">
                     <span className="label-text">Email</span>
                   </label>
+                  <span>Your name</span>
+                  <input
+                  
+                    type="text"
+                    placeholder="Your name" name="name"
+                    className="input input-bordered" required
+                  />
+                  <br />
                   <input
                     type="email"
                     placeholder="email" name="email"
@@ -61,10 +90,20 @@ const HeroRegister = () => {
                     <span className="label-text">Password</span>
                   </label>
                   <input
-                    type="password"
-                    placeholder="password" name="password"
+                    type={showPassword ?"text" :"password"}
+                    placeholder="password"
+                     name="password"
                     className="input input-bordered"
                   />
+                  <span className="absulate mb-3 ms-72 mt-[-32px]" onClick={()=> setShowPassword(!showPassword)}>
+                    {
+                        showPassword ? <FaEyeSlash></FaEyeSlash> :<FaEye></FaEye>
+                    }
+                  </span>
+                  <div className="flex gap-2"> 
+                  <input type="checkbox" name="checkbox" id="terms" />
+                  <label htmlFor="terms">Accept Our terms and condition</label>
+                  </div>
                   <label className="label">
                     <a href="#" className="label-text-alt link link-hover">
                       Forgot password?
@@ -72,7 +111,7 @@ const HeroRegister = () => {
                   </label>
                 </div>
                 <div className="form-control mt-6">
-                  <button className="btn btn-primary">Login</button>
+                  <button className="btn btn-primary">Sign up</button>
                 </div>
               </form>
               {
@@ -81,6 +120,7 @@ const HeroRegister = () => {
               {
                 sucess && <p className="text-green-500">{sucess}</p>
               }
+              <p>Already hava an account? <Link to="/login">Please Log in.</Link> </p>
             </div>
           </div>
         </div>
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,112 @@
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../Firebase/firebase.config";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">Please Log in</h2>
+  const [registerError, setRegisterError] = useState("");
+  const [sucess, setsucess] = useState("");
+  const emailRef = useRef(null) 
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    
+    //reset
+    setRegisterError('');
+    setsucess('')
+    // set validation
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        if(result.user.emailVerified){
+            setsucess("Login sucessfull");
+        }
+        else{
+            alert('please verify your email')
+        }
+      })
+      .catch((error) => {
+        setRegisterError("Login failed");
+        console.error(error);
+      });
+  };
+  const handleForgetPassword = () =>{
+       const email = emailRef.current.value;
+       if(!email){
+        console.log('please provide an email',email)
+        return;
+       }
+       else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email))
+       {
+        console.log('please write a valid email')
+        return;
+       }
+    //    send validation
+    sendPasswordResetEmail(auth,email)
+    .then(()=>{
+        alert('please check your Email')
+    })
+    .catch(error =>
+        console.log(error))
+  }
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <form onSubmit={handleLogin}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  ref={emailRef}
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a onClick={handleForgetPassword} href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+            {registerError && (
+              <h2 className="text-red-600">{registerError} </h2>
+            )}
+            {sucess && <p className="text-green-500">{sucess}</p>}
+            <p>New to this website <Link to="/heroregister">please register </Link> </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
